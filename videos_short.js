ffff86b5d0b1f534cca08c25f3f558facd519a41
videyaxhub <videya1x@gmail.com>
2025-09-12 10:54:06 +0700

    Update videos_short.js

--- videos_short.js
+++ videos_short.js
@@ -1,170 +1,34 @@
 const videosShort = [
-  {
-    "title": "He looks like an inner peace that contradicts reality \ud83d\ude2d",
-    "src": "https://cdn.videy.co/pJUHLkt51.mp4"
-  },
-  {
-    "title": "Nutted a simple caption \ud83d\ude09",
-    "src": "https://cdn.videy.co/EGFcCPEp1.mp4"
-  },
-  {
-    "title": "Oh She's Having Her Way",
-    "src": "https://cdn.videy.co/7uJnxSnX1.mp4"
-  },
-  {
-    "title": "what we do at girls night",
-    "src": "https://cdn.videy.co/krDRMmFI1.mp4"
-  },
-  {
-    "title": "Hang in there even it\u2019s hard to stay grounded",
-    "src": "https://cdn.videy.co/HVikn9a81.mp4"
-  },
-  {
-    "title": "Nahh this beautiful gonna put you to sleep",
-    "src": "https://cdn.videy.co/y6E3IGEY1.mp4"
-  },
-  {
-    "title": "Pure Cinema",
-    "src": "https://cdn.videy.co/NaUNxzDd1.mp4"
-  },
-  {
-    "title": "Nigga started giggling like a lil girl\ud83d\ude02",
-    "src": "https://cdn.videy.co/8uUC37DP1.mp4"
-  },
-  {
-    "title": "True friends help each other",
-    "src": "https://cdn.videy.co/dRScbvL91.mp4"
-  },
-  {
-    "title": "What Happens at The After Hour Kickback",
-    "src": "https://cdn.videy.co/t4DgE8X31.mp4"
-  },
-  {
-    "title": "You did that with a straight face?",
-    "src": "https://qu.ax/JqpGC.mp4"
-  },
-  {
-    "title": "Feeling hot and stuffy need a refresher",
-    "src": "https://cdn.videy.co/wYi48TVx1.mp4"
-  },
-  {
-    "title": "ampus Was Quiet, But Not Our Room...",
-    "src": "https://qu.ax/WcKGj.mp4"
-  },
-  {
-    "title": "Blue haired white girl getting plowed \ud83e\udee3\ud83e\udd2b",
-    "src": "https://cdn.videy.co/NohhKoHG1.mp4"
-  },
-  {
-    "title": "This hair so soft \ud83d\ude0d",
-    "src": "https://cdn.videy.co/902qMCED1.mp4"
-  },
-  {
-    "title": "I need a quick vacay \ud83d\ude29\ud83c\udf46",
-    "src": "https://cdn.videy.co/hlijS7M41.mp4"
-  },
-  {
-    "title": "Heavy duty smashed a nut for milk \ud83c\udf6f",
-    "src": "https://cdn.videy.co/meYaxZ721.mp4"
-  },
-  {
-    "title": "vomited after doing it..",
-    "src": "https://cdn.videy.co/fm9CTevk1.mp4"
-  },
-  {
-    "title": "Nutted before it even broken \ud83e\udd5c",
-    "src": "https://cdn.videy.co/xf8oJJVF1.mp4"
-  },
-  {
-    "title": "twins but both like each other",
-    "src": "https://cdn.videy.co/nR06BwLN1.mp4"
-  },
-  {
-    "title": "Outside be givin\u2019 energy y\u2019all ain\u2019t ready for",
-    "src": "https://cdn.videy.co/xQrsVFUG1.mp4"
-  },
-  {
-    "title": "so happy in front of the cams. shawty didn't care at all",
-    "src": "https://cdn.videy.co/PpwarT1i1.mp4"
-  },
-  {
-    "title": "Back aisle energy hittin different, who ridin\u2019 wit me tho? \ud83d\ude0f",
-    "src": "https://cdn.videy.co/ypGcX02K1.mp4"
-  },
-  {
-    "title": "I know he be having a time \ud83e\udd2d\ud83d\udcab",
-    "src": "https://cdn.videy.co/dnh0f2RE1.mp4"
-  },
-  {
-    "title": "Why it movin like it got beef wit gravity??",
-    "src": "https://cdn.videy.co/qa8cpVUt1.mp4"
-  },
-  {
-    "title": "Drip so loud, even the lollipop jealous",
-    "src": "https://cdn.videy.co/HNBrBlPb1.mp4"
-  },
-  {
-    "title": "jenise had the whole field on lock \ud83d\ude0c\ud83d\ude44",
-    "src": "https://cdn.videy.co/diIs7T4m1.mp4"
-  },
-  {
-    "title": "She movin\u2019 like rent due tomorrow frfr \ud83d\udc83\ud83c\udffd\u2728",
-    "src": "https://cdn.videy.co/qLvxKUsP1.mp4"
-  },
-  {
-    "title": "Ice Cream machine was down",
-    "src": "https://cdn.videy.co/kRymrbEy1.mp4"
-  },
-  {
-    "title": "That\u2019s a lot, can y\u2019all guess what it is?",
-    "src": "https://cdn.videy.co/vIJtUbZb1.mp4"
-  },
-  {
-    "title": "She said \u2018trust me\u2019\u2026 now I\u2019m halfway to Narnia \ud83d\udc80\ud83e\ude9c",
-    "src": "https://cdn.videy.co/8Mv9s5fz1.mp4"
-  },
-  {
-    "title": "Y\u2019all know scrubs don\u2019t always mean healthcare",
-    "src": "https://cdn.videy.co/hZVXkhEN1.mp4"
-  },
-  {
-    "title": "this magic trick works",
-    "src": "https://cdn.videy.co/jI9Npkgl1.mp4"
-  },
-  {
-    "title": "Walmart Employee on Break\ud83d\ude2e\u200d\ud83d\udca8",
-    "src": "https://cdn.videy.co/H2JOevbT1.mp4"
-  },
-  {
-    "title": "back blown, mark show\ud83d\ude2e\u200d\ud83d\udca8",
-    "src": "https://cdn.videy.co/c49m0jv41.mp4"
-  },
-  {
-    "title": "enough to make you weak!",
-    "src": "https://cdn.videy.co/lTQwNMZ01.mp4"
-  },
-  {
-    "title": "Whole vibe got ruined when moms kicked in \ud83d\ude29\ud83d\udc80",
-    "src": "https://cdn.videy.co/MnKgKKyj1.mp4"
-  },
-  {
-    "title": "Phone buzzin\u2019 like it owe me somethin\u2019",
-    "src": "https://cdn.videy.co/9GpM672v1.mp4"
-  },
-  {
-    "title": "Whole aisle turned into a runway\u2014she walked like she meant it",
-    "src": "https://cdn.videy.co/PCpGA1Vi1.mp4"
-  },
-  {
-    "title": "This the kinda pretty that make traffic stop",
-    "src": "https://cdn.videy.co/edhWodl51.mp4"
-  },
-  {
-    "title": "his one\u2019s got me like \u2018wait, WHAT?",
-    "src": "https://cdn.videy.co/pjwmPwyC1.mp4"
-  },
-  {
-    "title": "I'm thirsty and hungry\ud83d\ude0b\ud83d\ude0c",
-    "src": "https://cdn.videy.co/P4XsMvTt1.mp4"
-  }
+  { title: "He looks like an inner peace that contradicts reality 😭", url: "https://cdn.videy.co/pJUHLkt51.mp4" },
+  { title: "Nutted a simple caption 😉", url: "https://cdn.videy.co/EGFcCPEp1.mp4" },
+  { title: "Oh She's Having Her Way", url: "https://cdn.videy.co/7uJnxSnX1.mp4" },
+  { title: "what we do at girls night", url: "https://cdn.videy.co/krDRMmFI1.mp4" },
+  { title: "Hang in there even it’s hard to stay grounded", url: "https://cdn.videy.co/HVikn9a81.mp4" },
+  { title: "Nahh this beautiful gonna put you to sleep", url: "https://cdn.videy.co/y6E3IGEY1.mp4" },
+  { title: "Pure Cinema", url: "https://cdn.videy.co/NaUNxzDd1.mp4" },
+  { title: "Nigga started giggling like a lil girl😂", url: "https://cdn.videy.co/8uUC37DP1.mp4" },
+  { title: "True friends help each other", url: "https://cdn.videy.co/dRScbvL91.mp4" },
+  { title: "What Happens at The After Hour Kickback", url: "https://cdn.videy.co/t4DgE8X31.mp4" },
+  { title: "You did that with a straight face?", url: "https://qu.ax/JqpGC.mp4" },
+  { title: "Feeling hot and stuffy need a refresher", url: "https://cdn.videy.co/wYi48TVx1.mp4" },
+  { title: "Campus Was Quiet, But Not Our Room...", url: "https://qu.ax/WcKGj.mp4" },
+  { title: "Blue haired white girl getting plowed 🫣🤫", url: "https://cdn.videy.co/NohhKoHG1.mp4" },
+  { title: "This hair so soft 😍", url: "https://cdn.videy.co/902qMCED1.mp4" },
+  { title: "I need a quick vacay 😩🍆", url: "https://cdn.videy.co/hlijS7M41.mp4" },
+  { title: "Heavy duty smashed a nut for milk 🍯", url: "https://cdn.videy.co/meYaxZ721.mp4" },
+  { title: "vomited after doing it..", url: "https://cdn.videy.co/fm9CTevk1.mp4" },
+  { title: "Nutted before it even broken 🥜", url: "https://cdn.videy.co/xf8oJJVF1.mp4" },
+  { title: "twins but both like each other", url: "https://cdn.videy.co/nR06BwLN1.mp4" },
+  { title: "Outside be givin’ energy y’all ain’t ready for", url: "https://cdn.videy.co/xQrsVFUG1.mp4" },
+  { title: "so happy in front of the cams. shawty didn't care at all", url: "https://cdn.videy.co/PpwarT1i1.mp4" },
+  { title: "Back aisle energy hittin different, who ridin’ wit me tho? 😏", url: "https://cdn.videy.co/ypGcX02K1.mp4" },
+  { title: "I know he be having a time 🤭💫", url: "https://cdn.videy.co/dnh0f2RE1.mp4" },
+  { title: "Why it movin like it got beef wit gravity??", url: "https://cdn.videy.co/qa8cpVUt1.mp4" },
+  { title: "Drip so loud, even the lollipop jealous", url: "https://cdn.videy.co/HNBrBlPb1.mp4" },
+  { title: "jenise had the whole field on lock 😌🙄", url: "https://cdn.videy.co/diIs7T4m1.mp4" },
+  { title: "She movin’ like rent due tomorrow frfr 💃🏽✨", url: "https://cdn.videy.co/qLvxKUsP1.mp4" },
+  { title: "Ice Cream machine was down", url: "https://cdn.videy.co/kRymrbEy1.mp4" },
+  { title: "That’s a lot, can y’all guess what it is?", url: "https://cdn.videy.co/vIJtUbZb1.mp4" },
+  { title: "She said ‘trust me’… now I’m halfway to Narnia 💀🪜", url: "https://cdn.videy.co/8Mv9s5fz1.mp4" },
+  { title: "Y’all know scrubs don’t always mean healthcare", url: "https://cdn.videy.co/hZVXkhEN1.mp4" }
 ];
