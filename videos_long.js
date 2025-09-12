18153f5f0fea0c74531cb8c35ec80dd00c72e3f8
videyaxhub <videya1x@gmail.com>
2025-09-12 10:56:04 +0700

    Update videos_long.js

--- videos_long.js
+++ videos_long.js
@@ -1,162 +1,42 @@
 const videosLong = [
-  {
-    "title": "Auntie sleepin like she the dream I been tryna catch",
-    "src": "https://cdn.videy.co/eXR3LYr91.mp4"
-  },
-  {
-    "title": "Just me, the couch, and a storm of thoughts",
-    "src": "https://cdn.videy.co/UaWk9f371.mp4"
-  },
-  {
-    "title": "Y\u2019all want coffee or compliments? Cuz I\u2019m servin both",
-    "src": "https://cdn.videy.co/oHlRqQXt1.mp4"
-  },
-  {
-    "title": "Can anyone come help her?\ud83e\udee4\ud83d\ude44",
-    "src": "https://cdn.videy.co/Uy5OoDJU1.mp4"
-  },
-  {
-    "title": "Even your dreams didn't see this coming!",
-    "src": "https://cdn.videy.co/cflkrsX91.mp4"
-  },
-  {
-    "title": "Catching You Up on What Happened Last Night",
-    "src": "https://qu.ax/GUrQF.mp4"
-  },
-  {
-    "title": "Real Love Never Fades, It Lives in Our Memories",
-    "src": "https://qu.ax/lHVZX.mp4"
-  },
-  {
-    "title": "Little freshness, big difference",
-    "src": "https://qu.ax/bPkFX.mp4"
-  },
-  {
-    "title": "Collecting Little Moments of Happiness",
-    "src": "https://qu.ax/lZoJr.mp4"
-  },
-  {
-    "title": "Ageless wonder\u2026 Melting my thoughts away.",
-    "src": "https://cdn.videy.co/Y39ZyMV71.mp4"
-  },
-  {
-    "title": "She really said: \u201cYou thought this was still PG?\u201d",
-    "src": "https://cdn.videy.co/bacYouZf1.mp4"
-  },
-  {
-    "title": "Stronger Every Day, No Matter the Age",
-    "src": "https://qu.ax/SruTG.mp4"
-  },
-  {
-    "title": "This surpasses every expectation you've ever held",
-    "src": "https://cdn.videy.co/Fr9UXpzI1.mp4"
-  },
-  {
-    "title": "Just a Casual Day, Until My Bestie Showed Up!",
-    "src": "https://qu.ax/xUUJA.mp4"
-  },
-  {
-    "title": "Just Us, Just Relaxing \ud83d\ude0d",
-    "src": "https://qu.ax/wFKqP.mp4"
-  },
-  {
-    "title": "As calm and bright as the endless blue sky.",
-    "src": "https://qu.ax/hoden.mp4"
-  },
-  {
-    "title": "Wait\u2026 Is This Really You",
-    "src": "https://cdn.videy.co/ZrUH2kQN1.mp4"
-  },
-  {
-    "title": "One Day, the Right Heart Will Find Mine",
-    "src": "https://qu.ax/vSzLF.mp4"
-  },
-  {
-    "title": "At first he was embarrassed but then he enjoyed it.",
-    "src": "https://cdn.videy.co/j1jYpR671.mp4"
-  },
-  {
-    "title": "I\u2019m stunned. Shocked. Shooketh",
-    "src": "https://cdn.videy.co/6s1kulFN1.mp4"
-  },
-  {
-    "title": "This is why I can\u2019t focus in class someone\u2019s daughter be looking like this",
-    "src": "https://cdn.videy.co/q6B6YU5m1.mp4"
-  },
-  {
-    "title": "The Lady in Red Couldn\u2019t Resist \ud83d\udd25",
-    "src": "https://cdn.videy.co/e1BnGwaI1.mp4"
-  },
-  {
-    "title": "Alone but never bored \ud83d\udd25",
-    "src": "https://cdn.videy.co/rfaSO2VX1.mp4"
-  },
-  {
-    "title": "This presentation already stole my full attention \u2764\ufe0f",
-    "src": "https://qu.ax/lFJxz.mp4"
-  },
-  {
-    "title": "She makes every moment brighter!",
-    "src": "https://cdn.videy.co/5OfusNis1.mp4"
-  },
-  {
-    "title": "Comment it u like",
-    "src": "https://cdn.videy.co/H04aRQXb1.mp4"
-  },
-  {
-    "title": "u starting early \ud83d\ude0b",
-    "src": "https://cdn.videy.co/mxfIlurk1.mp4"
-  },
-  {
-    "title": "Nutted before it even got in",
-    "src": "https://cdn.videy.co/BbHcXuJo1.mp4"
-  },
-  {
-    "title": "Have to try she has.. Chocolate",
-    "src": "https://cdn.videy.co/luhOHPc61.mp4"
-  },
-  {
-    "title": "Even your dreams didn't see this coming!...",
-    "src": "https://qu.ax/Haluh.mp4"
-  },
-  {
-    "title": "she had receipts,but the streets got footage \ud83d\udc40\ud83c\udfa5",
-    "src": "https://cdn.videy.co/CNJiyMKc1.mp4"
-  },
-  {
-    "title": "Man\u2019s movin like he got secrets in that box \ud83d\udce6\ud83d\ude33",
-    "src": "https://cdn.videy.co/v8MytFTU1.mp4"
-  },
-  {
-    "title": "Backseat Banter Level: Legendary\ud83d\ude12",
-    "src": "https://cdn.videy.co/APtcpzv71.mp4"
-  },
-  {
-    "title": "Whole car feel like a reality show rn \ud83e\udd26\ud83c\udffe\u200d\u2642\ufe0f\ud83d\ude02",
-    "src": "https://cdn.videy.co/Tvs4eQPF1.mp4"
-  },
-  {
-    "title": "I\u2019m tryna get sentenced to life \ud83d\ude29\ud83d\udcaf\ud83d\udc6e\ud83c\udffe\u200d\u2640\ufe0f",
-    "src": "https://cdn.videy.co/wd9Lolh81.mp4"
-  },
-  {
-    "title": "She got that \u2018come clean this mess\u2019 energy \ud83d\ude0f\ud83e\uddf9",
-    "src": "https://cdn.videy.co/09nCL1o11.mp4"
-  },
-  {
-    "title": "the type of hg I need",
-    "src": "https://cdn.videy.co/Dpo1ZotY1.mp4"
-  },
-  {
-    "title": "Tag someone who\u2019s sweeter than Godiva",
-    "src": "https://cdn.videy.co/Wk6eLMXr1.mp4"
-  },
-  {
-    "title": "Head lecturer",
-    "src": "https://cdn.videy.co/w2U9aQVq1.mp4"
-  },
-  {
-    "title": "When the vibes hit record mode",
-    "src": "https://cdn.videy.co/02NElcy71.mp4"
-  }
+  { title: "Auntie sleepin like she the dream I been tryna catch", url: "https://cdn.videy.co/eXR3LYr91.mp4" },
+  { title: "Just me, the couch, and a storm of thoughts", url: "https://cdn.videy.co/UaWk9f371.mp4" },
+  { title: "Y’all want coffee or compliments? Cuz I’m servin both", url: "https://cdn.videy.co/oHlRqQXt1.mp4" },
+  { title: "Can anyone come help her?🫤🙄", url: "https://cdn.videy.co/Uy5OoDJU1.mp4" },
+  { title: "\"Even your dreams didn't see this coming!\"", url: "https://cdn.videy.co/cflkrsX91.mp4" },
+  { title: "Catching You Up on What Happened Last Night", url: "https://qu.ax/GUrQF.mp4" },
+  { title: "Real Love Never Fades, It Lives in Our Memories", url: "https://qu.ax/lHVZX.mp4" },
+  { title: "Little freshness, big difference", url: "https://qu.ax/bPkFX.mp4" },
+  { title: "Collecting Little Moments of Happiness", url: "https://qu.ax/lZoJr.mp4" },
+  { title: "Ageless wonder… Melting my thoughts away.", url: "https://cdn.videy.co/Y39ZyMV71.mp4" },
+  { title: "She really said: “You thought this was still PG?”", url: "https://cdn.videy.co/bacYouZf1.mp4" },
+  { title: "Stronger Every Day, No Matter the Age", url: "https://qu.ax/SruTG.mp4" },
+  { title: "This surpasses every expectation you've ever held", url: "https://cdn.videy.co/Fr9UXpzI1.mp4" },
+  { title: "Just a Casual Day, Until My Bestie Showed Up!", url: "https://qu.ax/xUUJA.mp4" },
+  { title: "Just Us, Just Relaxing 😍", url: "https://qu.ax/wFKqP.mp4" },
+  { title: "As calm and bright as the endless blue sky.", url: "https://qu.ax/hoden.mp4" },
+  { title: "Wait… Is This Really You", url: "https://cdn.videy.co/ZrUH2kQN1.mp4" },
+  { title: "One Day, the Right Heart Will Find Mine", url: "https://qu.ax/vSzLF.mp4" },
+  { title: "At first he was embarrassed but then he enjoyed it.", url: "https://cdn.videy.co/j1jYpR671.mp4" },
+  { title: "I’m stunned. Shocked. Shooketh", url: "https://cdn.videy.co/6s1kulFN1.mp4" },
+  { title: "This is why I can’t focus in class someone’s daughter be looking like this", url: "https://cdn.videy.co/q6B6YU5m1.mp4" },
+  { title: "The Lady in Red Couldn’t Resist 🔥", url: "https://cdn.videy.co/e1BnGwaI1.mp4" },
+  { title: "Alone but never bored 🔥", url: "https://cdn.videy.co/rfaSO2VX1.mp4" },
+  { title: "This presentation already stole my full attention ❤️", url: "https://qu.ax/lFJxz.mp4" },
+  { title: "She makes every moment brighter!", url: "https://cdn.videy.co/5OfusNis1.mp4" },
+  { title: "Comment it u like", url: "https://cdn.videy.co/H04aRQXb1.mp4" },
+  { title: "u starting early 😋", url: "https://cdn.videy.co/mxfIlurk1.mp4" },
+  { title: "Nutted before it even got in", url: "https://cdn.videy.co/BbHcXuJo1.mp4" },
+  { title: "Have to try she has.. Chocolate", url: "https://cdn.videy.co/luhOHPc61.mp4" },
+  { title: "Even your dreams didn't see this coming!...", url: "https://qu.ax/Haluh.mp4" },
+  { title: "she had receipts,but the streets got footage 👀🎥", url: "https://cdn.videy.co/CNJiyMKc1.mp4" },
+  { title: "Man’s movin like he got secrets in that box 📦😳", url: "https://cdn.videy.co/v8MytFTU1.mp4" },
+  { title: "Backseat Banter Level: Legendary😒", url: "https://cdn.videy.co/APtcpzv71.mp4" },
+  { title: "Whole car feel like a reality show rn 🤦🏾‍♂️😂", url: "https://cdn.videy.co/Tvs4eQPF1.mp4" },
+  { title: "I’m tryna get sentenced to life 😩💯👮🏾‍♀️", url: "https://cdn.videy.co/wd9Lolh81.mp4" },
+  { title: "She got that ‘come clean this mess’ energy 😏🧹", url: "https://cdn.videy.co/09nCL1o11.mp4" },
+  { title: "the type of hg I need", url: "https://cdn.videy.co/Dpo1ZotY1.mp4" },
+  { title: "Tag someone who’s sweeter than Godiva", url: "https://cdn.videy.co/Wk6eLMXr1.mp4" },
+  { title: "Head lecturer", url: "https://cdn.videy.co/w2U9aQVq1.mp4" },
+  { title: "When the vibes hit record mode", url: "https://cdn.videy.co/02NElcy71.mp4" }
 ];
